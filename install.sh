be353407ea10e6cfffe4914a7cdfa1e5fa350528
zardaa <75716772+zardaa@users.noreply.github.com>
2020-12-18 18:05:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-apt update && apt upgrade
+apt update
+apt upgrade
+apt install
+pkg update && pkg upgrade
 pkg install wget
 pkg install ffmpeg
 pkg install nodejs
+pkg install tesseract
 npm i -g cwebp
 npm i -g ytdl 
 npm i
